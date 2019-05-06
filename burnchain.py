ee6bc60143d259f917a2bdf0e61e7ab98afb413f
Srinjoy Chakravarty <chakravarty.s@husky.neu.edu>
2019-05-06 19:56:46 +0530

    secure key generation + digital signing

--- burnchain.py
+++ burnchain.py
@@ -1,13 +1,27 @@
-#RSA Signatures
+from cryptography.hazmat.backends import default_backend                        #RSA Signatures with default backend
+from cryptography.hazmat.primitives.asymmetric import rsa                       #Generates new RSA private key with provided backend
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 def generate_keys():
-    priv_key = 'aksjdlmqwaepqooew9ios30qpowadsw'
-    pub_key = 'w9023qeujwd93oqlwqwd[qea;d]'
+    priv_key = rsa.generate_private_key(                                        #RSA keys have complex internal structure with specific mathematical properties.
+                                        public_exponent=65537,                  #65537 is the smallest public_exponent known to prevent Coppersmith's Attack for short unpadded messages, providing efficient signature verification
+                                        key_size=4096,                          #key_size of 4096 bits provide most security
+                                        backend=default_backend()               #implements RSABackend.
+                                       )
+    pub_key = priv_key.public_key()
     return priv_key, pub_key
 
-def digitally_sign(message, priv_key):
-    digital_sig = '7y<dahsniew'
-    return digital_sig
+def generate_signature(message, private_key):
+    digital_signature = private_key.sign(
+                                message,
+                                padding.PSS(
+                                            mgf=padding.MGF1(hashes.SHA256()),
+                                            salt_length=padding.PSS.MAX_LENGTH  #PSS is the recommended pseudorandom salt for padding new protocols
+                                            ),
+                                hashes.SHA256()                                 #RSA signatures require SHA256 hash
+                                )
+    return digital_signature
 
 def verify(unsigned_msg, signed_msg, pub_key):
     return False
@@ -16,12 +30,15 @@ def verify(unsigned_msg, signed_msg, pub_key):
 
 if __name__ == '__main__':
     privateTestKey, publicTestKey = generate_keys()
+    print(privateTestKey)
+    print(publicTestKey)
     originalMessage = b"Aguero is actually not blonde"
-    signedMessage = digitally_sign(originalMessage, privateTestKey)
-    passedCorrectnessCheck = verify(originalMessage, signedMessage, publicTestKey)
+    digitalSignature = generate_signature(originalMessage, privateTestKey)
+    print(digitalSignature)
+    passedCorrectnessCheck = verify(originalMessage, digitalSignature, publicTestKey)
     if passedCorrectnessCheck:
         print("Digital Signature Valid!")
     else:
-        print("Warning! Corrupt Signature Detected!")
+        print("Warning! Invalid Signature Detected!")
 
     print ("Python3 Detected!")
